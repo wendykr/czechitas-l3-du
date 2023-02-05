3dcaf21f0ff36fed3a0c46006a14cb17067412ac
Vendula Krajíčková <vendysacek@seznam.cz>
2023-02-05 18:49:42 +0100

    Hotový úkol

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# czechitas-l3-du
+# Vstup od uživatele - Lekce 3
+
+Domácí úkol pro kurz JavaScript 1 od Czechitas.
+
+
+## Zadání
+
+**1.** Vytvoř program, který se zeptá uživatele ("vstup"), na jeho Jméno, Příjmení a rok narození. Na výstupu vypiš jeho Jméno Příjmení a Věk.
+
+*<i> Filip Procházka 30 </i>* - ukázkový výstup
+
+
+**2.** Přidej do programu "vstup" uživatele s jeho oblíbenou barvou ("anglicky") s tím, že celý výpis s úkolu č. 1. obarvíš danou barvou, ze vstupu uživatele.
+
+**"vstup" = funkce `prompt()`**
--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 // tady je místo pro náš program
 
+let vysledek = document.querySelectorAll('.uzivatel');
+let tlacitko = document.querySelectorAll('button');
 
+function zobrazitText () {
+    let jmeno = prompt('Zadejte své jméno:');
 
+    if (jmeno == '') {
+        alert('Jméno je povinný údaj');
+    } else {
+        let prijmeni = prompt('Zadejte své příjmení:');
 
+        if (prijmeni == '') {
+            alert('Příjmení je povinný údaj');
+        } else {
+            const rok = Number(prompt('Zadejte svůj rok narození:'));
+            const vek = new Date().getFullYear() - rok;
+            if (vek == '' || vek == new Date().getFullYear()) {
+                alert('Rok je povinný údaj');
+            } else {
+                for (i = 0; i < vysledek.length; i++) {
+                    vysledek[i].innerHTML = '<em>' + jmeno + ' ' + prijmeni + ' ' + vek + '</em>';
+                }
+                tlacitko[1].style.visibility = ('visible');
+            }
+        }
+    }
+}    
+
+function obarvitText () {
+    let color = prompt('Zadejte svou oblíbenou barvu:');
+
+    for (i = 0; i < vysledek.length; i++) {
+        vysledek[i].style.color = color;
+    }
+    vysledek[1].style.visibility = ('visible');
+}
